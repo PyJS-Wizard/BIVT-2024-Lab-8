2df9da78d0e303a4a198ef2a3abd876a3b8f7826
Mark Kalinkin <mark.kalinkin.07@gmail.com>
2025-03-21 23:25:15 +0300

    Add formatting of nums

--- Lab_8/Purple.cs
+++ Lab_8/Purple.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace Lab_8 {
     public abstract class Purple {
         private string _input;
         protected static readonly char[] punctuationMarks 
-            = {'.', '!', '?', ',', ':', '\\', ';', '–', '(', ')', '[', ']', '{', '}', '/'};
+            = {'.', '!', '?', ',', ':', '\"', ';', '–', '(', ')', '[', ']', '{', '}', '/', '-'}; /// REMOVE! "-"
         public string Input => _input;
 
         public Purple(string input) {
@@ -23,6 +25,40 @@ namespace Lab_8 {
 
         protected bool IsInWord(char c) {
             return char.IsLetter(c) || c == '-' || c == '\''; 
+        }
+
+        public string FormatNumbers(string s) {
+            if (string.IsNullOrEmpty(s)) return s;
+                
+            var result = new StringBuilder();
+            int i = 0;
+            
+            while (i < s.Length) {
+
+                if (char.IsDigit(s[i])) {
+                    int start = i;
+                    bool hasDecimalPoint = false;
+                    
+                    for (; i < s.Length; i++) {
+                        if ((s[i] == '.' || s[i] == ',') && !hasDecimalPoint) 
+                            hasDecimalPoint = true;
+                        else if (!char.IsDigit(s[i]))
+                            break;
+                    }
+                    
+                    string numberStr = s[start..i];
+                    string formattedString = numberStr;
+
+                    if ((i < s.Length && (punctuationMarks.Contains(s[i]) || s[i] == ' ')) || (i == s.Length))
+                        formattedString = double.Parse(numberStr).ToString("f4");
+
+                    result.Append(formattedString);
+
+                } else result.Append(s[i++]);
+
+            }
+            
+            return result.ToString();
         }
     }
 }
